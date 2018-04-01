d34f287f2a2d0d4f79f0f33f5e5c3f77a946ba55
Sergei <sergei.visotsky@gmail.com>
2018-04-01 21:21:50 +0300

    LinkedQueue test done up to the end, VectorQueue test methods renamed as conventionall methodNameTest

--- src/test/java/Ld3_30/LinkedQueueTest.java
+++ src/test/java/Ld3_30/LinkedQueueTest.java
@@ -1,5 +1,78 @@
+package Ld3_30;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedQueueTest {
+    private LinkedQueue linkedQueue = new LinkedQueue();
+    private int iterator = 10;
 
+    @Test
+    public void QueIsEmptyTest() {
+        assertTrue(linkedQueue.isEmpty());
+        assertEquals(0, linkedQueue.getSize());
+    }
+
+    @Test
+    public void enQueueEmptyQueueTest() {
+        for (int i = 0; i < iterator; i++) {
+            linkedQueue.enQueue(i);
+        }
+
+        assertTrue(!linkedQueue.isEmpty());
+        assertEquals(iterator, linkedQueue.getSize());
+    }
+
+    @Test
+    public void enqueueThanDequeueTest() {
+        int number = 80;
+        linkedQueue.enQueue(number);
+        assertEquals(linkedQueue.deQueue(), number);
+    }
+
+    @Test
+    public void enqueueThanPeekTest() {
+        int number = 90;
+        int size = linkedQueue.getSize();
+        assertEquals(number, linkedQueue.peek());
+        assertEquals(size, linkedQueue.getSize());
+    }
+
+    @Test
+    public void TenInAndThenTenOutTest() {
+        for (int i = 0; i < iterator; i++) {
+            linkedQueue.enQueue(i);
+        }
+
+        for (int i = 0; i < iterator; i++) {
+            assertEquals(i, ((Integer) linkedQueue.deQueue()).intValue());
+        }
+    }
+
+    @Test
+    public void removingDownToEmpty() {
+        int numberOfRemoves = (int) (Math.random() * 20 + 1);
+        for (int i = 0; i < numberOfRemoves; i++) {
+            linkedQueue.enQueue(80);
+        }
+        for (int i = 0; i < numberOfRemoves; i++) {
+            linkedQueue.deQueue();
+        }
+
+        assertTrue(linkedQueue.isEmpty());
+        assertEquals(0, linkedQueue.getSize());
+    }
+
+    @Test
+    public void removingOnEmptyQueueTest() {
+        assertTrue(linkedQueue.isEmpty());
+        linkedQueue.deQueue();
+    }
+
+    @Test
+    public void peekIntoEmptyQueueTest() {
+        assertTrue(linkedQueue.isEmpty());
+        linkedQueue.deQueue();
+    }
 }
--- src/test/java/Ld3_30/VectorQueueTest.java
+++ src/test/java/Ld3_30/VectorQueueTest.java
@@ -8,13 +8,13 @@ class VectorQueueTest {
     protected VectorQueue queue = new VectorQueue(10);
 
     @Test
-    public void testNewQueueIsEmpty() {
+    public void newQueueIsEmptyTest() {
         assertTrue(queue.isEmpty());
         assertEquals(queue.getSize(), 0);
     }
 
     @Test
-    public void testInsertsToEmptyQueue() {
+    public void insertsToEmptyQueueTest() {
         int numberOfInserts = 6;
         for (int i = 0; i < numberOfInserts; i++) {
             queue.enQueue(40);
@@ -24,14 +24,14 @@ class VectorQueueTest {
     }
 
     @Test
-    public void testEnqueueThenDequeue() {
+    public void enqueueThenDequeueTest() {
         int number = 19;
         queue.enQueue(number);
         assertEquals(queue.deQueue(), number);
     }
 
     @Test
-    public void testEnqueueThenPeek() {
+    public void enqueueThenPeekTest() {
         int number = 19;
         queue.enQueue(number);
         int size = queue.getSize();
@@ -40,7 +40,7 @@ class VectorQueueTest {
     }
 
     @Test
-    public void testFiftyInThenFiftyOut() {
+    public void tenInThenTenOutTest() {
         for (int i = 0; i < 10; i++) {
             queue.enQueue(i);
         }
@@ -50,7 +50,7 @@ class VectorQueueTest {
     }
 
     @Test
-    public void testRemovingDownToEmpty() {
+    public void removingDownToEmptyTest() {
         int numberOfRemoves = (int) (Math.random() * 20 + 1);
         for (int i = 0; i < numberOfRemoves; i++) {
             queue.enQueue(19);
@@ -64,14 +64,14 @@ class VectorQueueTest {
 
     //    @Test(expected=NoSuchElementException.class)
     @Test
-    public void testRemoveOnEmptyQueue() {
+    public void removeOnEmptyQueueTest() {
         assertTrue(queue.isEmpty());
         queue.deQueue();
     }
 
     //    @Test(expected=NoSuchElementException.class)
     @Test
-    public void testPeekIntoEmptyQueue() {
+    public void peekIntoEmptyQueueTest() {
         assertTrue(queue.isEmpty());
         queue.deQueue();
     }
