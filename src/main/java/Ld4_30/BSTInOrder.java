ac598a65d8a24ffab892f2f05af441fc805edf31
Sergei <sergei.visotsky@gmail.com>
2018-04-21 17:02:08 +0300

    BST with inorder output added

--- src/main/java/Ld4_30/BSTInOrder.java
+++ src/main/java/Ld4_30/BSTInOrder.java
@@ -1,4 +1,71 @@
 package Ld4_30;
 
-public class BSTInOrder {
+interface IBinarySearchTreeTwo {
+    void insert(int element);
+
+    void inorderOutput();
+
+    int countNodesWithTwoChildren();
+
+    int zeroElementQuantity();
+
+    int getSize();
+
+    boolean isEmpty();
+
+    boolean isFull();
+}
+
+public class BSTInOrder implements IBinarySearchTreeTwo {
+    class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    @Override
+    public void insert(int element) {
+
+    }
+
+    @Override
+    public void inorderOutput() {
+
+    }
+
+    @Override
+    public int countNodesWithTwoChildren() {
+        return 0;
+    }
+
+    @Override
+    public int zeroElementQuantity() {
+        return 0;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean isFull() {
+        return size == 15;
+    }
+
+
 }
--- src/test/java/Ld4_30/BSTInOrderTest.java
+++ src/test/java/Ld4_30/BSTInOrderTest.java
@@ -1,3 +1,5 @@
+package Ld4_30;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BSTInOrderTest {
