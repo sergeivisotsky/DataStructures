ce0ba4a15c7559dda03956ac888f9f7ab0cfb4ab
Sergei <sergei.visotsky@gmail.com>
2018-04-26 20:35:29 +0300

    Code refactoring

--- src/main/java/Abstract/MainObject.java
+++ src/main/java/Abstract/MainObject.java
@@ -2,23 +2,33 @@ package Abstract;
 
 interface IStudentState {
     void working();
+
     void going();
+
     void relaxing();
 }
 
-public class MainObject implements IStudentState {
-    @Override
-    public void working() {
-        System.out.println("Student is working too hard doing math!");;
-    }
+public class MainObject {
+    public static void main(String[] args) {
+        IStudentState student = new IStudentState() {
+            @Override
+            public void working() {
+                System.out.println("Student is working too hard doing math!");
+            }
 
-    @Override
-    public void going() {
-        System.out.println("Student is going to the gym!");
-    }
+            @Override
+            public void going() {
+                System.out.println("Student is going to the gym!");
+            }
 
-    @Override
-    public void relaxing() {
-        System.out.println("Student is watching movie in the cinema!");
+            @Override
+            public void relaxing() {
+                System.out.println("Student is watching movie in the cinema!");
+            }
+        };
+
+        student.working();
+        student.going();
+        student.relaxing();
     }
 }
--- src/main/java/Abstract/MainObjectTwo.java
+++ src/main/java/Abstract/MainObjectTwo.java
@@ -2,24 +2,33 @@ package Abstract;
 
 abstract class StudentLife {
     abstract void working();
+
     abstract void going();
+
     abstract void relaxing();
 }
 
-public class MainObjectTwo extends StudentLife {
+public class MainObjectTwo {
+    public static void main(String[] args) {
+        StudentLife student = new StudentLife() {
+            @Override
+            void working() {
+                System.out.println("Student is working too hard doing math!");
+            }
 
-    @Override
-    void working() {
-        System.out.println("Student is working too hard doing math!");
-    }
+            @Override
+            void going() {
+                System.out.println("Student is going to the gym!");
+            }
 
-    @Override
-    void going() {
-        System.out.println("Student is going to the gym!");
-    }
+            @Override
+            void relaxing() {
+                System.out.println("Student is watching movie in the cinema!");
+            }
+        };
 
-    @Override
-    void relaxing() {
-        System.out.println("Student is watching movie in the cinema!");
+        student.working();
+        student.going();
+        student.relaxing();
     }
 }
