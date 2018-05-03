8d1c6159edf20c3f8adca766f83cdbe04175cf58
Sergei <sergei.visotsky@gmail.com>
2018-05-03 17:27:04 +0300

    C/C++ data structure modification

--- src/main/java/Abstract/MainDS.cpp
+++ src/main/java/Abstract/MainDS.cpp
@@ -1,21 +1,38 @@
+#include "stdafx.h"
 #include <string>
+#include <list>
 #include <iostream>
 
 using namespace std;
-struct Student {
-    string name;
-    string surname;
-    int age;
-    string studentNumber;
-    string groupNumber;
+struct student_struct {
+	string name;
+	string surname;
+	int age;
+	string student_number;
+	string group_number;
+
+	student_struct(string _name, string _surname, int _age,
+		string _student_number, string _group_number) {
+		name = _name;
+		surname = _surname;
+		age = _age;
+		student_number = _student_number;
+		group_number = _group_number;
+	}
 };
 
-void main() {
-	list<Student> students;
-	students.push_front(new Student(""))
-    Student.name = "John";
-    Student.surname = "Kensington";
-    Student.age = 19;
-    Student.studentNumber = "111rdb111";
-    Student.groupNumber = "IRDBD00";
+int main() {
+	list<student_struct> students;
+	students.push_front(student_struct("John", "Smith", 23, "171rdb042", "IRDBD13"));
+	students.push_front(student_struct("Lilia", "Quick", 19, "171rdb056", "IRDBD10"));
+	students.push_front(student_struct("Johanna", "Bloom", 20, "171rdb045", "IRDBD02"));
+	for (auto student : students)
+	{
+		cout << student.name << "\t";
+		cout << student.surname << "\t";
+		cout << student.age << "\t";
+		cout << student.student_number << "\t";
+		cout << student.group_number << "\t" << endl;
+	}
+	return 0;
 };
