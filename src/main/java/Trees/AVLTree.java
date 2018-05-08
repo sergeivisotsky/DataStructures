3ea4e09890b6e4f48fbba3feeea0ea2ed60a064c
Sergei <sergei.visotsky@gmail.com>
2018-05-08 13:56:03 +0300

    AVL tree interface and main operation functions added

--- src/main/java/Trees/AVLTree.java
+++ src/main/java/Trees/AVLTree.java
@@ -1,6 +1,23 @@
 package Trees;
 
-class AVLTreeClass {
+interface IAVLTree {
+    void insert(int data);
+
+    AVLTreeClass.Node leftRotate(int data);
+    AVLTreeClass.Node rightRotate(int data);
+
+    int find(int data);
+
+    void delete(int data);
+
+    int getSize();
+
+    boolean isEmpty();
+
+//    boolean isFull();
+}
+
+class AVLTreeClass implements IAVLTree {
     class Node {
         int data;
         int height;
@@ -13,7 +30,45 @@ class AVLTreeClass {
         }
     }
 
-    
+    private Node root;
+    private int size = 0;
+
+    @Override
+    public void insert(int data) {
+
+    }
+
+    @Override
+    public Node leftRotate(int data) {
+        return null;
+    }
+
+    @Override
+    public Node rightRotate(int data) {
+        return null;
+    }
+
+    @Override
+    public int find(int data) {
+        return 0;
+    }
+
+    @Override
+    public void delete(int data) {
+
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
 }
 
 public class AVLTree {
--- src/test/java/Trees/AVLTreeTest.java
+++ src/test/java/Trees/AVLTreeTest.java
@@ -3,5 +3,5 @@ package Trees;
 import static org.junit.Assert.*;
 
 public class AVLTreeTest {
-
+    private AVLTreeClass AVLTree = new AVLTreeClass();
 }
