3b6671aae7c0b34fba1c0bc0bc71030799fffe8a
Sergei <sergei.visotsky@gmail.com>
2018-05-08 13:49:37 +0300

    Class for Node added

--- src/main/java/Trees/AVLTree.java
+++ src/main/java/Trees/AVLTree.java
@@ -2,8 +2,18 @@ package Trees;
 
 class AVLTreeClass {
     class Node {
+        int data;
+        int height;
+        Node left;
+        Node right;
 
+        public Node(int data) {
+            this.data = data;
+            height = 1;
+        }
     }
+
+    
 }
 
 public class AVLTree {
